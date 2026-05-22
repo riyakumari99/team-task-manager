ede24bc0c4e3964c4e4ec98a135d0ab6a7a1269d
Riya Kumari <singhriya99733@gmail.com>
2026-05-22 14:33:08 +0530

    temp rename

--- server/routes/projectRoutes.js
+++ server/routes/projectRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const auth = require("../middleware/authMiddleware");
+const auth = require("../middleware/temp");
 const roleMiddleware = require("../middleware/roleMiddleware");
 
 const {
