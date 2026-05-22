e0d0133a8eaa750b045e665469da739292b97d74
Riya Kumari <singhriya99733@gmail.com>
2026-05-22 14:34:28 +0530

    fixed middleware filename

--- server/routes/projectRoutes.js
+++ server/routes/projectRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const auth = require("../middleware/temp");
+const auth = require("../middleware/authMiddleware");
 const roleMiddleware = require("../middleware/roleMiddleware");
 
 const {
--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const authMiddleware = require("../middleware/temp");
+const authMiddleware = require("../middleware/authMiddleware");
 const roleMiddleware = require("../middleware/roleMiddleware");
 
 const {
